9e4aabd556963c4573d03fab9c0e1a9497203799
yqchilde <yqchilde@users.noreply.github.com>
2024-05-06 14:34:38 +0800

    Deploying to gh-pages from @ yqchilde/yqchilde.github.io@0a901917b2e63ace7cadc2e0b0fb5878455221aa 🚀

--- assets/chunks/redirect.vue_vue_type_script_setup_true_lang.BE7Dpnii.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./framework.BKaFImy5.js";const i=e({__name:"redirect",props:{route:String},setup(o){const r=o;return window.location.href=window.location.href+r.route,(n,t)=>null}});export{i as _};
