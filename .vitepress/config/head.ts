a2e112ad951c51f1c26f34e28b562e2792444eb3
yqchilde <yqchilde@gmail.com>
2024-08-12 23:38:26 +0800

    fix: head development bug

--- .vitepress/config/head.ts
+++ .vitepress/config/head.ts
@@ -1,6 +1,7 @@
 import type { HeadConfig } from 'vitepress'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
+console.log({ src: isDevelopment ? '' : 'https://hm.baidu.com/hm.js?28d2c15f874fe87dc4038ad4d40b8a29' });
 
 export const head: HeadConfig[] = [
   ['meta', { name: 'theme-color', content: '#3eaf7c' }],
@@ -11,8 +12,5 @@ export const head: HeadConfig[] = [
   ['meta', { name: 'baidu-site-verification', content: 'codeva-a9OBeZwEU6' }],
   ['link', { rel: 'apple-touch-icon', href: '/favicon.ico' }],
   ['link', { rel: 'mask-icon', href: '/favicon.ico', color: '#3eaf7c' }],
-  [
-    'script',
-    { src: isDevelopment ? '' : 'https://hm.baidu.com/hm.js?28d2c15f874fe87dc4038ad4d40b8a29' }
-  ]
+  ...(isDevelopment ? [] : [['script', { src: 'https://hm.baidu.com/hm.js?28d2c15f874fe87dc4038ad4d40b8a29' }]] as HeadConfig[]),
 ]
