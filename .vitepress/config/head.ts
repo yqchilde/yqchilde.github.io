f538be594447ece53ff14e92369f478c36ab05d9
yqchilde <yqchilde@gmail.com>
2024-08-13 08:23:10 +0800

    fix: typo

--- .vitepress/config/head.ts
+++ .vitepress/config/head.ts
@@ -1,7 +1,6 @@
 import type { HeadConfig } from 'vitepress'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
-console.log({ src: isDevelopment ? '' : 'https://hm.baidu.com/hm.js?28d2c15f874fe87dc4038ad4d40b8a29' });
 
 export const head: HeadConfig[] = [
   ['meta', { name: 'theme-color', content: '#3eaf7c' }],
